2c7b1fa048b8f9e314992aaf2e9c0d7e99110284
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-10-05 01:39:14 -0400

    Changes to the connection.js and server.js files in order to see if I can finally deploy to Heroku.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const hbs = exphbs.create({helpers});
 
@@ -49,5 +49,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(port, () => console.log('Now listening'));
 });
